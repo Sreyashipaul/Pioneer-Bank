0a7a4ab81e9f07561a350c9dd090a4c4421dd2a3
Sreyashi paul <sreyoshisreyo@gmail.com>
2020-08-01 22:29:31 +0600

    2nd changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function insertNumber(number){
-    alert(number);
-}
+//login button event handler
+
+const loginBtn=document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+     const loginArea = document.getElementById("login-area");
+     loginArea.style.display = "none";
+     const transctionArea = document.getElementById("transaction-area");
+     transctionArea.style.display = "block";
+})
+
+
